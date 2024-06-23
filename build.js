3e0fca71c223d3f6fe3641560f759d579ce7a746
kookyleo <87705880+kookyleo@users.noreply.github.com>
2024-06-23 11:28:57 +0800

    update

---
+++ build.js
@@ -0,0 +1,11 @@
+// "build": "npm run build-base -- --sourcemap",
+// "watch": "tsc -watch -p ./",
+
+require('esbuild').build({
+    entryPoints: ['src/extension.ts'],
+    bundle: true,
+    platform: 'node',
+    target: 'node12', // 根据 VS Code 的最低 Node.js 版本需求
+    outfile: 'extension.js',
+    external: ['vscode'], // VS Code 的模块不应被打包
+  }).catch(() => process.exit(1))
